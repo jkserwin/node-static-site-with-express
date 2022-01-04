c67649c0731c6ec7cfb73b3bf8ca75382da0cb08
Jake Serwin <88554771+jkserwin@users.noreply.github.com>
2022-01-04 13:12:47 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { links } = require('express/lib/response');
+const data = require('./data.json');
+const { projects } = data;
+
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const templateData = { id, projectName, description, technologies, liveLink, githubLink };
+    res.render('project', templateData);
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
