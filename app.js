25aa4f2881beed7f2333402d1839c196fca3dc79
Jake Serwin <88554771+jkserwin@users.noreply.github.com>
2022-01-07 11:07:43 -0800

    added data for projects and error handlers

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const { links } = require('express/lib/response');
 const data = require('./data.json');
@@ -10,7 +11,7 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { projects });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +22,23 @@ app.get('/projects/:id', (req, res) => {
     const { id } = req.params;
     const project = projects[id];
     res.render('project', project);
+});
+
+// 404 error handler
+
+app.use((req, res, next) => {
+    const err = new Error('404: Page not found');
+    err.status = 404;
+    console.error("404: Page not found");
+    next(err);
+});
+
+// Global error handler
+
+app.use((err, req, res, next) => {
+    err.message = err.message || `Oops! Looks like there was a server error: ${err.status}.`;
+    res.status(err.status || 500);
+    next(err);
 });
 
 app.listen(3000, () => {
